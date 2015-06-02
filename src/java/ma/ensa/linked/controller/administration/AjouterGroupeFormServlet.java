ce3496fcdffb75e9afbcbcf6d829194f9783a82d
Mohammed Amine OUASMINE <m.amine.osm@gmail.com>
2015-06-02 16:02:22 +0100

    # 10 nothing

--- src/java/ma/ensa/linked/controller/administration/AjouterGroupeFormServlet.java
+++ src/java/ma/ensa/linked/controller/administration/AjouterGroupeFormServlet.java
@@ -6,7 +6,6 @@
 package ma.ensa.linked.controller.administration;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
